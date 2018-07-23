951fbb6fe5c45db4eb3f400c665771abd7ff8e66
Sam Ashtar <samashtarm@gmail.com>
2018-07-23 20:13:07 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,7 @@
-def meal_choice (meal_choice = "meat")
+def choice(meal_choice="meat")
+    puts "The Meal Choice is #{meal_choice}!"
 end
+
+def meal_choice(name = "meat")
+  meal_choice = "#{name}"
+end 
